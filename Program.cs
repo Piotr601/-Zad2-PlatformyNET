1cf3ea3028cb35a13ee78d2ac59b4389d98b3e46
Filip Kurasz <33543681+fkurasz@users.noreply.github.com>
2021-03-29 13:31:49 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Zad2
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+            //string response = await client.GetStringAsync("http://radoslaw.idzikowski.staff.iiar.pwr.wroc.pl/instruction/students.json");
+            string response = await client.GetStringAsync("https://openexchangerates.org/api/latest.json?app_id=984511ca2af843e59f5229a272a16bec");
+            Console.WriteLine(response);
+            //984511ca2af843e59f5229a272a16bec
         }
+        
     }
 }
