d43e744b3f992cad9472e724e51d3f200086658c
Piotr601 <piotr.niedziolka601@gmail.com>
2021-04-05 19:42:04 +0200

    v5

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Newtonsoft.Json;
 using System.Threading;
 using System.Data.Entity;
 using System.Linq;
+using System.Windows;
 
 namespace Zad2
 {
@@ -12,11 +13,6 @@ namespace Zad2
     {
 
         static ICurrency context = new ICurrency();
-        // METHODS
-        /*
-         * 
-         * 
-         */
 
         public static void Start()
         {
@@ -32,30 +28,58 @@ namespace Zad2
             Console.WriteLine("\n #-------=================================-------#");
             Console.WriteLine(" |                   M E N U                     |");
             Console.WriteLine(" #-------=================================-------#");
-            Console.WriteLine(" |    1) Podaj walute    (PLN, EUR, JPY, ...)    |");
-            Console.WriteLine(" |       (Domyslnie dzisiaj)                     |");
+            Console.WriteLine(" |    1) Wybierz walute    (PLN, EUR, JPY, ...)  |");
+            Console.WriteLine(" |       (Domyslna data - dzisiaj)               |");
             Console.WriteLine(" |-----------------------------------------------|");
-            Console.WriteLine(" |    2) Podaj date      (RRRR-MM-DD)            |");
-            Console.WriteLine(" |       (Domyslnie PLN)                         |");
+            Console.WriteLine(" |    2) Wybierz date      (RRRR-MM-DD)          |");
+            Console.WriteLine(" |       (Domyslna waluta - PLN)                 |");
             Console.WriteLine(" |-----------------------------------------------|");
-            Console.WriteLine(" |    3) Podaj walute i date                     |");
+            Console.WriteLine(" |    3) Wybierz walute i date                   |");
             Console.WriteLine(" |-----------------------------------------------|");
             Console.WriteLine(" |    4) Pokaz baze danych                       |");
             Console.WriteLine(" |-----------------------------------------------|");
             Console.WriteLine(" |    5) Wyczysc baze danych                     |");
             Console.WriteLine(" |-----------------------------------------------|");
+            Console.WriteLine(" |    6) Wyswietl ponownie menu                  |");
+            Console.WriteLine(" |-----------------------------------------------|");
             Console.WriteLine(" |    0) # Wyjscie z programu #                  |");
             Console.WriteLine(" #-------=================================-------#\n");
         }
 
-        public static void showAllDB()
-        {
+
+
+        public static bool showAllDB()
+        {         
             var waluty = (from s in context.Waluta select s).ToList<Currency>();
-            foreach (var st in waluty)
+            int i = 0;
+            bool found = false;
+            bool wypisalo = false;
+            foreach(var st in waluty) //po kazdym
             {
-                Console.WriteLine($"ID: {st.ID}, Waluta: {st.waluta}, Kurs: {st.kurs}, Dzien: {st.dzien}");
+                foreach (var iter in waluty) //po kazdym do ST
+                {
+                                        if (iter.ID >= st.ID) //doszlismy lub przekroczylismy ST
+                        break;
+                    else //od 0 do ST
+                    {
+                        if (st.waluta == iter.waluta && st.dzien == iter.dzien) // jesli st jest tym samym co iter (czyli wiersz w ktorym jestesmy juz sie wczesniej pojawil)
+                        {
+                            found = true;
+                            break;  // to koniec i wybieramy nastepne st
+                        }
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine($" | Waluta: {st.waluta}\t Kurs: " + String.Format("{0:N4}", st.kurs) + $"\t Dzien: {st.dzien}"); // nie pojawil sie to wypisujemy
+                    wypisalo = true;
+                }
+                found = false;
             }
+            return wypisalo;
         }
+
+
 
         public static async void GetAPI(string _data, string _waluta)
         {
@@ -80,6 +104,7 @@ namespace Zad2
                 Console.ForegroundColor = ConsoleColor.White;
             }
         }
+
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.White;
@@ -159,7 +184,6 @@ namespace Zad2
                         }
                     }
 
-
                 }
                 if (wybor == 3)
                 {
@@ -218,7 +242,12 @@ namespace Zad2
                 }
                 if(wybor==4)
                 {
-                    showAllDB();
+                    if (!showAllDB())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("!! Pusta baza danych !!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
                 }
                 if(wybor==5)
                 {
@@ -230,8 +259,14 @@ namespace Zad2
                     foreach (var record in records)
                         context.Waluta.Remove(record);
                     context.SaveChanges();
+
                 }
-                if(wybor>5 || wybor < 0)
+                if(wybor == 6)
+                {
+                    Console.Clear();
+                    Menu();
+                }
+                if(wybor > 6 || wybor < 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("!! Wybierz poprawnie !!");
